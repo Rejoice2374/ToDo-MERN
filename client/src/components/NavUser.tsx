881ceebaf99b8a9832b7594e7449b7d815dbc742
Favochino <adeboyerejoice2374@gmail.com>
2026-04-18 23:59:47 +0100

    feat: restructure user dropdown menu to improve layout and organization

--- client/src/components/NavUser.tsx
+++ client/src/components/NavUser.tsx
@@ -39,11 +39,11 @@ const NavUser = ({ user }: Userprops) => {
           <DropdownMenuTrigger aschild>
             <SidebarMenuButton size="lg" aschild>
               <div className="flex items-center gap-2">
-                <Avatar className="h-8 w-8 rounded-lg">
+                <Avatar className="h-8 w-full rounded-lg">
                   <AvatarImage src={user.avatar} alt={user.firstName} />
                   <AvatarFallback className="rounded-lg">
                     {user?.firstName?.[0]}
-                    {user?.lastName?.charAt(0)}
+                    {user?.lastName?.[0]}
                   </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
@@ -62,20 +62,25 @@ const NavUser = ({ user }: Userprops) => {
             align="start"
             sideOffset={4}
           >
-            <DropdownMenuLabel className="p-0 font-normal">
-              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.firstName} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-                </Avatar>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">
-                    {user.firstName} {user.lastName}
-                  </span>
-                  <span className="truncate text-xs">{user.email}</span>
+            <DropdownMenuGroup>
+              <DropdownMenuLabel className="p-0 font-normal">
+                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                  <Avatar className="h-8 w-8 rounded-lg">
+                    <AvatarImage src={user.avatar} alt={user.firstName} />
+                    <AvatarFallback className="rounded-lg">
+                      {user?.firstName?.charAt(0)}
+                      {user?.lastName?.charAt(0)}
+                    </AvatarFallback>
+                  </Avatar>
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-medium">
+                      {user.firstName} {user.lastName}
+                    </span>
+                    <span className="truncate text-xs">{user.email}</span>
+                  </div>
                 </div>
-              </div>
-            </DropdownMenuLabel>
+              </DropdownMenuLabel>
+            </DropdownMenuGroup>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
               <DropdownMenuItem>
