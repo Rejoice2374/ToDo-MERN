9ced5973a9c616ae7dad84a01d70c0c6c4d127a0
Favochino <adeboyerejoice2374@gmail.com>
2026-05-05 16:31:26 +0100

    Started incorporating some dashboard components into the dashboard page

--- client/src/components/DashboardStat.tsx
+++ client/src/components/DashboardStat.tsx
@@ -1,5 +1,37 @@
+import { Separator } from "@/components/ui/separator"
+import CategoryChart from "./CategoryChart"
+import LongestStreak from "./LongestStreak"
+
 const DashboardStat = () => {
-  return <div>DashboardStat</div>
+  return (
+    <div className="flex flex-1 flex-col gap-4 p-8 md:px-10 md:py-6">
+      <h2 className="text-2xl font-semibold">Dashboard</h2>
+      <Separator />
+      <div className="flex flex-col space-y-4">
+        <div className="flex items-center justify-between">
+          <h2 className="text-xl font-semibold">Basic Habits Stats</h2>
+        </div>
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+          {/* Categories Chart */}
+          <CategoryChart />
+          {/* Longest streak */}
+          <LongestStreak />
+        </div>
+      </div>
+      <Separator />
+      <div className="flex flex-col space-y-4">
+        <div className="flex items-center justify-between">
+          <h2 className="text-xl font-semibold">Habits Insights</h2>
+        </div>
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+          {/* Categories Chart */}
+          <CategoryChart />
+          {/* Longest streak */}
+          <LongestStreak />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default DashboardStat
