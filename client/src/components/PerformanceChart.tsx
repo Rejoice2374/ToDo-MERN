6d739cdcace52dcbaf434594703731250951385e
Favochino <adeboyerejoice2374@gmail.com>
2026-05-07 06:23:06 -0400

    Remove redundant imports

--- client/src/components/PerformanceChart.tsx
+++ client/src/components/PerformanceChart.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { TrendingUp } from "lucide-react"
-import { Label, Pie, PieChart } from "recharts"
+import { Pie, PieChart } from "recharts"
 import {
   Card,
   CardContent,
